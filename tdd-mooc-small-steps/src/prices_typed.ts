1f013c0e228b4a4bc0d5e4fe353323fb009e0c8a
Ismo Mehdi <84803085+ismomehdi@users.noreply.github.com>
2025-02-02 22:18:07 +0200

    tests pass

--- tdd-mooc-small-steps/src/prices_typed.ts
+++ tdd-mooc-small-steps/src/prices_typed.ts
@@ -94,10 +94,10 @@ function createApp(database: Database) {
       let holiday = new Date(row.holiday);
       let holiday2 = Temporal.PlainDate.from(row.holiday)
       if (
-        date && date2 &&
+        date2 &&
         date2.year === holiday2.year &&
         date2.month === holiday2.month &&
-        date.getDate() === holiday.getDate()
+        date2.day === holiday2.day
       ) {
         return true;
       }
